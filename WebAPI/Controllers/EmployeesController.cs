72ca748245bc444f11549ac62149ad408882d8b0
M Imran <88980084+programmer4nd@users.noreply.github.com>
2024-02-10 18:40:12 +0300

    WebAPI

---
+++ WebAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.ViewModels;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<EmployeeVM> GetAll()
+        {
+            IEnumerable<EmployeeVM> _data = new List<EmployeeVM>
+            {
+                new EmployeeVM()
+                {
+                    Id = 1,
+                    Name = "Imran",
+                    Department = "IT",
+                    JobTitle = "Senior Dot Net Develope",
+                    Email = "imran@test.com",
+                },
+                new EmployeeVM()
+                {
+                    Id = 2,
+                    Name = "Asad",
+                    Department = "IT",
+                    JobTitle = "Senior Mobile Develope",
+                    Email = "asad@test.com",
+                },
+                new EmployeeVM()
+                {
+                    Id = 3,
+                    Name = "Jabar",
+                    Department = "IT",
+                    JobTitle = "Project Manager",
+                    Email = "jabar@test.com",
+                },
+                new EmployeeVM()
+                {
+                    Id = 4,
+                    Name = "Marie",
+                    Department = "HR",
+                    JobTitle = "HR Exective",
+                    Email = "marie@test.com",
+                },
+                new EmployeeVM()
+                {
+                    Id = 5,
+                    Name = "Shama",
+                    Department = "Admin",
+                    JobTitle = "Admin",
+                    Email = "shama@test.com",
+                }
+            };
+            return _data;
+        }
+    }
+}
---
+++ WebAPI/ViewModels/EmployeeVM.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.ViewModels
+{
+    public class EmployeeVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string JobTitle { get; set; }
+        public string Email { get; set; }
+        
+    }
+}
